ccf1edd7f07a843be2a343bfffb5671fedef535c
Athyla Guilherme Jeronimo Pires <athylaguilherme20@gmail.com>
2022-09-19 20:55:51 -0300

    Model Centro de Custo e Tipo

--- app/Models/CentroCusto.php
+++ app/Models/CentroCusto.php
@@ -4,8 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+// Models
+use App\Models\{Tipo};
 
 class CentroCusto extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'centro_custos';
+    protected $primarykey = 'id_centro_custo';
+    protected $dates = ['created_at','updated_at','deleted_at'];
+    protected $fillable = ['id_tipo', 'centro_custo'];
+
+
+    /**
+     * |------------------------------------------------------------------|
+     * | Relacionamentos                                                  |
+     * | https://laravel.com/docs/9.x/eloquent-relationships#main-content |
+     * |------------------------------------------------------------------|
+     */
+
+     /**
+      * Retorna o tipo do centro de custos
+      * 19-02-2022
+      * @return object
+      */
+
+     public function tipo()
+     {
+        return $this->belongsTo(Tipo::class,'id_tipo','id_tipo');
+     }
 }
