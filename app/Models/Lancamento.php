e041ff7d8f09c3639eff0ecb7313a79942bcdd29
Athyla Guilherme Jeronimo Pires <athylaguilherme20@gmail.com>
2022-09-19 21:06:14 -0300

    Model Lançamento

--- app/Models/Lancamento.php
+++ app/Models/Lancamento.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+//Models
+use App\Models\CentroCusto, user;
 
 class Lancamento extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'lancamentos';
+    protected $primaryKey = 'id_lancamento';
+    protected $dates = ['dt_faturamento', 'created_at', 'update_at', 'deleted_at'];
+    protected $fillable = ['id_user', 'id_centro_custo','descricao','obsevacoes'];
+
+    /**
+     * |------------------------------------------------------------------|
+     * | Relacionamentos                                                  |
+     * | https://laravel.com/docs/9.x/eloquent-relationships#main-content |
+     * |------------------------------------------------------------------|
+     */
+
+     public function usuario()
+     {
+        return $this->belongsTo(User::class,'id_user','id_user');
+     }
+
+     public function centroCusto()
+     {
+        return $this->belongsTo(CentroCusto::class,'id_centro_custo','id_centro_custo');
+     }
 }
